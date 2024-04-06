bb9ae3b63f24b0e6afcb5f6e812a6ecab0ab98b6
technOrbit <maheshrahane65@gmail.com>
2024-04-06 07:40:05 +0530

    To learn Dynamic components.

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
 
 const Products = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2 heading="PRODUCTS." text="Some of my team, most recent works.." />
       <Footer />
     </div>
   )
